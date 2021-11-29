794bbe72bed8251ba9b0772d1f7301008ef661c5
kasylozy <kasylozy@gmail.com>
2021-11-29 10:41:51 -0500

    update maildev

--- install.sh
+++ install.sh
@@ -288,6 +288,17 @@ EOF
   sudo chown root:root $touchpadFile
 fi
 
+if [ ! -f /usr/bin/docker ]; then
+  sudo pacman -S docker --noconfirm
+  if [ `systemctl is-enabled docker.service` = "disabled" ] ; then
+    sudo systemctl enable docker.service
+    sudo systemctl start docker.service
+  fi
+  if ! sudo docker ps | grep mail; then
+    sudo docker run -d --restart unless-stopped -p 1080:1080 -p 1025:1025 dominikserafin/maildev:latest
+  fi
+fi
+
 if [ ! -f /swapfile ]; then
   sudo truncate -s 0 /swapfile
   sudo chattr +C /swapfile
